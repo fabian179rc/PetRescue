f9ee67a786982dcae3c6a5bddf251c6535864935
Fabian179rc <fabian179rc@gmail.com>
2022-12-03 23:28:47 -0300

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# PetRescue
+# Huellitas
---
+++ api/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 9000;
+const MONGODB_URI = process.env.MONGODB_URI;
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user");
+
+app.listen(port, () => console.log("server listening on port", port));
+
+//mongoose connection
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
+
+//routes
+app.get("/", (req, res) => {
+  res.send("welcome API");
+});
+
+//middleware
+app.use(express.json());
+app.use("/", userRoutes);
